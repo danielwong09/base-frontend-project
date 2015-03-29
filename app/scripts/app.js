a22d0fff418b8a9ef502de701805753e77f893a3
Daniel Wong <daniel.wong09@gmail.com>
2015-03-29 22:40:48 +0000

    Defined blank home state via UI Router.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,18 @@
 
 var BlocChat = angular.module('BlocChat', ['ui.router']);
 
+BlocChat.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+
+  $locationProvider.html5Mode(true);
+
+  $stateProvider.state('home', {
+    url: '/',
+    controller: 'Home.controller',
+    templateUrl: '/templates/home.html'
+  });
+
+}]);
+
 BlocChat.controller('Home.controller', ['$scope', function($scope) {
-  console.log("home.controller");
- }]);
+  console.log("home.controller is connected");
+}]);
