b186880cf0a5aac9fbb17735cd041cb352853e2b
Daniel Wong <daniel.wong09@gmail.com>
2015-03-29 22:21:29 +0000

    Added UI router as angular app dependency. Separated angular app declaration from controller.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,6 @@
-angular.module('BlocChat', []).controller('Home.controller', ['$scope', function($scope) {
+
+var BlocChat = angular.module('BlocChat', ['ui.router']);
+
+BlocChat.controller('Home.controller', ['$scope', function($scope) {
   console.log("home.controller");
  }]);
