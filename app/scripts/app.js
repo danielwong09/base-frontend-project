3817fe293a363296ba480cfdc6548919773f76eb
Daniel Wong <daniel.wong09@gmail.com>
2015-04-02 03:42:34 +0000

    Created rooms factory with add function. Added to rooms to home controller scope. Added basic home controller add room function

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,5 @@
 
-var BlocChat = angular.module('BlocChat', ['ui.router']);
+var BlocChat = angular.module('BlocChat', ['ui.router', 'firebase']);
 
 BlocChat.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
 
@@ -13,6 +13,28 @@ BlocChat.config(['$stateProvider', '$locationProvider', function($stateProvider,
 
 }]);
 
-BlocChat.controller('Home.controller', ['$scope', function($scope) {
-  console.log("home.controller is connected");
+BlocChat.controller('Home.controller', ['$scope', 'Room', function($scope, Room) {
+  $scope.rooms = Room.all;
+  $scope.addRoom = function(e){
+  	Room.add({name: $scope.roomName});
+  };
 }]);
+
+// 'app' refers to a variable referencing your app's module definition
+BlocChat.factory('Room', ['$firebaseArray', function($firebaseArray) {
+
+	console.log("Room");
+	//https://mybloc-chat.firebaseio.com/#-JlNe2utRXaWYNZWTdU0|deedfda97257dab768c3547ecaada92f
+  var firebaseRef = new Firebase("https://mybloc-chat.firebaseio.com/");
+  var rooms = $firebaseArray(firebaseRef.child('rooms'));
+  rooms.$add({name:'daniel'});
+
+  return {
+    all: rooms,
+    add: function(newRoom) {
+       // Checks to make sure that a sound file is playing before seeking.
+       rooms.$add({ name: newRoom.name});
+     },
+  }
+
+}])
