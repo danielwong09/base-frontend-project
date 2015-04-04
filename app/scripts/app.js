28d1116051661d2ef646f878e379ec5e9f70065c
Daniel Wong <daniel.wong09@gmail.com>
2015-04-04 16:37:43 +0000

    Fixed listing rooms and adding rooms using firebaseArray

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,24 +17,24 @@ BlocChat.controller('Home.controller', ['$scope', 'Room', function($scope, Room)
   $scope.rooms = Room.all;
   $scope.addRoom = function(e){
   	Room.add({name: $scope.roomName});
+  	//clear form
+  	$scope.roomName = "";
   };
 }]);
 
-// 'app' refers to a variable referencing your app's module definition
 BlocChat.factory('Room', ['$firebaseArray', function($firebaseArray) {
-
-	console.log("Room");
-	//https://mybloc-chat.firebaseio.com/#-JlNe2utRXaWYNZWTdU0|deedfda97257dab768c3547ecaada92f
+	
   var firebaseRef = new Firebase("https://mybloc-chat.firebaseio.com/");
-  var rooms = $firebaseArray(firebaseRef.child('rooms'));
-  rooms.$add({name:'daniel'});
+
+  var rooms = $firebaseArray(firebaseRef.child('rooms'))
 
   return {
     all: rooms,
     add: function(newRoom) {
-       // Checks to make sure that a sound file is playing before seeking.
+
        rooms.$add({ name: newRoom.name});
+      
      },
   }
 
-}])
+}]);
